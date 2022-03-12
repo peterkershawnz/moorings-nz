dc630e1411e47067cac1016a287ab2e1d92da3d7
Peter Kershaw <peterkershaw@developersinstitute.co.nz>
2022-03-12 14:59:21 +1300

    update the api, add to the owner endpoint

--- pages/api/repository.js
+++ pages/api/repository.js
@@ -2,7 +2,7 @@ import Mooring from "../../models/Mooring";
 import Booking from "../../models/Booking";
 import User from "../../models/User";
 import Owner from "../../models/Owner";
-const { ObjectID } = require('mongodb');
+import { ObjectId } from 'mongodb'
 
 const getMooringsByLocation = async (lat, lng) => {
 
@@ -44,6 +44,30 @@ const getMooringsByLocationAndDates = async (lat, lng, from, to) => {
     return "lots of moorings";
 }
 
+const getMooringByNumber = async (number) => {
+    const pipeline = [
+        {
+            '$match': {
+                'properties.number': number
+            }
+        }, {
+            '$lookup': {
+                'from': 'owners',
+                'localField': 'properties.number',
+                'foreignField': 'mooring_number',
+                'as': 'mooring_owners'
+            }
+        }
+    ];
+
+    try {
+        const mooring = await Mooring.aggregate(pipeline);
+        return mooring[0];
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
 const createBooking = async (bookingData) => {
     // to be tested
     try {
@@ -64,34 +88,60 @@ const createUser = async (userDate) => {
     }
 }
 
-const createOwner = async (data) => {
+const createOwner = async ({ mooring_number, auth0 }) => {
     try {
-        const owner = await Owner.create({ ...data, created_date: new Date() });
+        const owner = await Owner.create({ mooring_number, auth0, created_date: new Date() });
         return owner;
     } catch (error) {
         throw new Error(error)
     }
 }
 
-const getOwner = async (id) => {
+const getOwnerById = async (id) => {
     const pipeline = [
         {
             '$match': {
-                '_id': ObjectID(id)
+                '_id': ObjectId(id)
             }
         }, {
             '$lookup': {
                 'from': 'moorings',
                 'localField': 'mooring_number',
                 'foreignField': 'properties.number',
-                'as': 'users_moorings'
+                'as': 'owner_moorings'
             }
         }
     ];
     try {
         const owner = await Owner.aggregate(pipeline);
-        // const owner = await Owner.aggregate(pipeline);
         return owner;
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const getOwnerByAuth0 = async ({ auth0 }) => {
+    try {
+        const owner = await Owner.findOne({ 'auth0': auth0 });
+        return owner;
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const updateOwnerById = async (id, mooring_number) => {
+    try {
+        const owner = await Owner.updateOne({ _id: ObjectId(id) }, { '$push': { 'mooring_number': mooring_number } });
+        return owner;
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+const deleteOwnerById = async (id) => {
+    try {
+        const deleteOwner = await Owner.deleteOne({ '_id': ObjectId(id) });
+        return deleteOwner;
     } catch (error) {
         throw new Error(error)
     }
@@ -100,8 +150,12 @@ const getOwner = async (id) => {
 module.exports = {
     getMooringsByLocation,
     getMooringsByLocationAndDates,
+    getMooringByNumber,
     createBooking,
     createUser,
     createOwner,
-    getOwner
+    getOwnerById,
+    getOwnerByAuth0,
+    updateOwnerById,
+    deleteOwnerById
 }
